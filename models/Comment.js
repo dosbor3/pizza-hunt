0b0697cc07f0307b36e242248cf8d9e96d94361f
I am Billie <dosbor3@gmail.com>
2022-03-27 10:04:02 -0500

    added reply feature

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,30 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');   //types used to create a custom ID field
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema ({
+    //set custom id field to avoid confusion with parent id field
+    replyID: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    replyBody: {
+        type: String
+    },
+    writtenBy: {
+        type: String
+    },    
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+{
+    toJSON: {
+        getters:true
+    }
+    
+});
 
 const CommentSchema = new Schema ({
     writtenBy: {
@@ -9,9 +35,31 @@ const CommentSchema = new Schema ({
     },
     createdAt: {
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    //use ReplySchema to validate data fro a reply
+    replies: [ReplySchema],
+},
+{
+    toJSON: {
+        getters: true, 
+        virtuals: true
+    },
+    id: false
 });
+
+/*
+Here we'll need a unique identifier instead of the default _id field that is created, so we'll add a custom replyId field. Despite the custom field name, we're still going to have it generate the same type of ObjectId() value that the _id field typically does, but we'll have to import that type of data first.
+
+At the top of Comment.js update the import code statement to look like this:
+*/
+
+
+// get total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+  });
 
 const Comment = model('Comment', CommentSchema);
 
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
 
 //Set up a route called /api/comments/:pizzaId and use the addComment() method as a POST callback. 
 // /api/comments/<pizzaId>
@@ -7,6 +7,12 @@ router.route('/:pizzaId').post(addComment);
 
 //Set up another route for /api/comments/:pizzaId/:commentId and use the removeComment method as a DELETE callback.
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+.route('/:pizzaId/:commentId')
+.put(addReply)
+.delete(removeComment);
+
+//removing comment
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
